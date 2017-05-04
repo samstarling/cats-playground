2caa1438d1cfcde609e9e13f5f5fabf346d76ec9
Sam Starling <sam@soundcloud.com>
2017-05-04 15:02:25 +0200

    Add a worked Either example in the form of ReciprocalService

--- src/main/scala/com/samstarling/ReciprocalExceptionService.scala
+++ src/main/scala/com/samstarling/ReciprocalExceptionService.scala
@@ -1,15 +1,16 @@
 package com.samstarling
 
-class SquareNumberExceptionService {
+import cats.syntax.either._
 
-  def parse(s: String): Int = {
-    if (s.matches("-?[0-9]+")) {
-      s.toInt
-    } else {
-      throw new NumberFormatException(s"${s} is not a valid integer.")
-    }
-  }
+class ReciprocalService {
 
-  def square(i: Int)
+  def parse(s: String): Either[Exception, Int] =
+    if (s.matches("-?[0-9]+")) Either.right(s.toInt)
+    else Either.left(new NumberFormatException(s"${s} is not a valid integer."))
 
+  def reciprocal(i: Int): Either[Exception, Double] =
+    if (i == 0) Either.left(new IllegalArgumentException("Cannot take reciprocal of 0."))
+    else Either.right(1.0 / i)
+
+  def stringify(d: Double): String = d.toString
 }
--- src/test/scala/com/samstarling/ReciprocalServiceSpec.scala
+++ src/test/scala/com/samstarling/ReciprocalServiceSpec.scala
@@ -1,5 +1,20 @@
 package com.samstarling
 
-class ReciprocalServiceSpec {
+import org.specs2.mutable.Specification
+
+import cats.syntax.either._
+
+class ReciprocalServiceSpec extends Specification {
+
+  val service = new ReciprocalService
+
+  "ReciprocalService" >> {
+    "can be used with some nice combinators" in {
+      service.parse("5").flatMap(service.reciprocal).map(service.stringify) match {
+        case Right(result) => result ==== "0.2"
+        case _ => ko
+      }
+    }
+  }
 
 }
