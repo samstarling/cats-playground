ea1e39734ef08f3734a3958ead830b1ac448f1b7
Sam Starling <sam@soundcloud.com>
2017-05-04 17:19:32 +0200

    Test that the ReciprocalService returns an exception

--- src/test/scala/com/samstarling/either/ReciprocalServiceSpec.scala
+++ src/test/scala/com/samstarling/either/ReciprocalServiceSpec.scala
@@ -1,6 +1,5 @@
 package com.samstarling.either
 
-import cats.syntax.either._
 import org.specs2.mutable.Specification
 
 class ReciprocalServiceSpec extends Specification {
@@ -8,12 +7,18 @@ class ReciprocalServiceSpec extends Specification {
   val service = new ReciprocalService
 
   "ReciprocalService" >> {
-    "can be used with some nice combinators" in {
+    "returns a Right with the result, when successful" in {
       service.apply("5") match {
         case Right(result) => result ==== "0.2"
         case _ => ko
       }
     }
-  }
 
+    "returns a Left with the exception, when one occurs" in {
+      service.apply("foo") match {
+        case Left(ex) => ex must haveClass[NumberFormatException]
+        case Right(result) => ko
+      }
+    }
+  }
 }
