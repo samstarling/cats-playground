536d9f53b16d4e543ad88369afce9cd27e8ed159
Sam Starling <sam@soundcloud.com>
2017-05-05 14:18:20 +0200

    Add annotation for FPIS exercises

--- src/main/scala/com/samstarling/monoid/EndoMonoid.scala
+++ src/main/scala/com/samstarling/monoid/EndoMonoid.scala
@@ -2,6 +2,7 @@ package com.samstarling.monoid
 
 import cats.Monoid
 
+// FPIS exercise 10.3
 class EndoMonoid[T] extends Monoid[T => T] {
   override def empty: (T) => T = (t: T) => t
   override def combine(x: (T) => T, y: (T) => T): (T) => T = x andThen y
