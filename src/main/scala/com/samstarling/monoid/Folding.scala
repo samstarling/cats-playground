a518849a23cf11f267e0bce6c25a81a2f87d2064
Sam Starling <sam@soundcloud.com>
2017-05-05 15:18:53 +0200

    Add alternative implementation of foldMap, placeholders for foldLeft and foldRight

--- src/main/scala/com/samstarling/monoid/Folding.scala
+++ src/main/scala/com/samstarling/monoid/Folding.scala
@@ -10,6 +10,12 @@ class Folding {
 
   // FPIS exercise 10.5
   def foldMap[A, B](as: List[A], monoid: Monoid[B])(f: A => B): B = {
-    as.map(f).foldLeft(monoid.empty)(monoid.combine)
+    as.foldLeft(monoid.empty)((b, a) => monoid.combine(b, f(a)))
   }
+
+  // FPIS exercise 10.6
+  def foldLeft[A, B](as: List[A])(z: B)(f: (B, A) => B): B = ???
+
+  // FPIS exercise 10.6 part 2
+  def foldRight[A, B](as: List[A])(z: B)(f: (A, B) => B): B = ???
 }
